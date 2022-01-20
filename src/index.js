7bc5c161dca287de0c4d22f39fe5b36a6c913468
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-01-20 11:14:17 +0530

    feat: #Routing Methods
    Implemented basic routing by responding to various HTTP methods on a particular endpoint. Used
    app.all() to listen to all methods on a particular end point to be used as a middleware.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-require('./basics');
-
+// require('./basics');
+require('./routing');
---
+++ src/routing/index.js
@@ -0,0 +1,15 @@
+require('./methods');
+require('./paths');
+require('./parameters');
+require('./handlers');
+require('./express-router');
+require('./app-route');
+
+/**
+ *  Route methods
+ *  Route paths
+ *  Route parameters
+ *  Route handlers
+ *  app.route()
+ *  Express.router()
+ */
